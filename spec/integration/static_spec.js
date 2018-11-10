63ac8f55c3f76cd241c60f84981ad3851451a24c
Jacob Levine <Jacob.levine93@protonmail.com>
2018-11-10 10:49:51 -0700

    Mentor Help

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -9,15 +9,21 @@ describe('routes : static', () => {
 			request.get(base, (err, res, body) => {
 				expect(res.statusCode).toBe(200);
 				done();
-			});
+			}).catch((err) => {
+				console.log(err);
+				done();
+			});			
 		});
 	});
 
 	describe('GET /marco', () => {
 		it('should return status code 200 and "polo" as the body', (done) => {
-			request.get(base, (err, res, body) => {
+			request.get('http://localhost:3000/marco', (err, res, body) => {
 				expect(res.statusCode).toBe(200);
 				expect(body).toBe('polo');
+				done();
+			}).catch((err) => {
+				console.log(err);
 				done();
 			});
 		});
