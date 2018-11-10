491a3787c56c25cc8ef21f02f5971f42ee56ad99
Jacob Levine <Jacob.levine93@protonmail.com>
2018-11-10 16:40:11 -0700

    WIP deploy to production

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const routerConfig = require('./config/router-config.js');
+const appConfig = requie('./config/main-config.js');
 
+appConfig.init();
 routerConfig.init(app);
 
 module.exports = app;
