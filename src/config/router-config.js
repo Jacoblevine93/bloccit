9a8f75e5bae0e5110e7ec246639c78b28fcb776f
Jacob Levine <Jacob.levine93@protonmail.com>
2018-11-17 14:12:03 -0700

    checkpoint 4 in progress

--- src/config/router-config.js
+++ src/config/router-config.js
@@ -1,6 +1,8 @@
 module.exports = {
 	init(app){
 		const staticRoutes = require('../routes/static');
+		const topiccRoutes = require('../routes/topics');
 		app.use(staticRoutes);
+		app.use(topiccRoutes);
 	}
 }
