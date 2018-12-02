8e0328e68833b15e28e056ec1c83e60e0eb9b0ab
Jacob Levine <Jacob.levine93@protonmail.com>
2018-12-01 17:55:22 -0700

    Assignment 5 Complete

--- src/config/router-config.js
+++ src/config/router-config.js
@@ -2,7 +2,9 @@ module.exports = {
 	init(app){
 		const staticRoutes = require('../routes/static');
 		const topiccRoutes = require('../routes/topics');
+		const advertisementRoutes = require('../routes/advertisements');
 		app.use(staticRoutes);
 		app.use(topiccRoutes);
+		app.use(advertisementRoutes);
 	}
 }
