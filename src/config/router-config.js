c7712cc67da0de0cb10e49c75d7974164cecea14
Jacob Levine <Jacob.levine93@protonmail.com>
2018-12-04 11:04:31 -0700

    Checkpoint 7 Complete

--- src/config/router-config.js
+++ src/config/router-config.js
@@ -2,7 +2,9 @@ module.exports = {
 	init(app){
 		const staticRoutes = require('../routes/static');
 		const topiccRoutes = require('../routes/topics');
+		const postRoutes = require('../routes/posts');
 		app.use(staticRoutes);
 		app.use(topiccRoutes);
+		app.use(postRoutes);
 	}
 }
