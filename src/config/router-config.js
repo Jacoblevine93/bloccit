18bb59730dba15b040d2fbe7b1d56965f7796edd
Jacob Levine <Jacob.levine93@protonmail.com>
2018-12-13 17:15:34 -0700

    Checkpoint 9 Complete

--- src/config/router-config.js
+++ src/config/router-config.js
@@ -3,8 +3,10 @@ module.exports = {
 		const staticRoutes = require('../routes/static');
 		const topiccRoutes = require('../routes/topics');
 		const postRoutes = require('../routes/posts');
+		const userRoutes = require("../routes/users");
 		app.use(staticRoutes);
 		app.use(topiccRoutes);
 		app.use(postRoutes);
+		app.use(userRoutes);
 	}
 }
