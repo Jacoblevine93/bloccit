1891ab0d18aabbf2b143034924f0301b4a426318
Jacob Levine <Jacob.levine93@protonmail.com>
2018-12-21 11:39:36 -0700

    Assignment 10 Complete

--- src/db/queries.posts.js
+++ src/db/queries.posts.js
@@ -24,34 +24,67 @@ module.exports = {
    },   
    
    deletePost(id, callback){
-     return Post.destroy({
-       where: { id }
-     })
-     .then((deletedRecordsCount) => {
-       callback(null, deletedRecordsCount);
+
+ // #1
+     return Post.findById(id)
+     .then((post) => {
+
+ // #2
+      const authorized = new Authorizer(req.user, post).destroy();       
+
+       if(authorized) {
+ // #3
+         post.destroy()
+         .then((res) => {
+           callback(null, post);
+         });
+         
+       } else {
+
+ // #4
+         req.flash("notice", "You are not authorized to do that.")
+         callback(401);
+       }
      })
      .catch((err) => {
        callback(err);
-     })
+     });
    },
 
    updatePost(id, updatedPost, callback){
+
+// #1
      return Post.findById(id)
      .then((post) => {
+
+// #2
        if(!post){
          return callback("Post not found");
        }
 
-       post.update(updatedPost, {
-         fields: Object.keys(updatedPost)
-       })
-       .then(() => {
-         callback(null, post);
-       })
-       .catch((err) => {
-         callback(err);
-       });
+// #3
+       const authorized = new Authorizer(req.user, post).update();
+
+       if(authorized) {
+
+// #4
+         post.update(updatedPost, {
+           fields: Object.keys(updatedPost)
+         })
+         .then(() => {
+           callback(null, post);
+         })
+         .catch((err) => {
+           callback(err);
+         });
+       } else {
+
+// #5
+         req.flash("notice", "You are not authorized to do that.");
+         callback("Forbidden");
+       }
      });
-   }       
+   }
+
 
 }
